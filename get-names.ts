c6004dff46436b2ae07c6091c9c7b7aa9e20dfbc
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-06-03 16:19:56 -0700

    Add documentation

--- get-names.ts
+++ get-names.ts
@@ -1,1 +1,3 @@
+// run this on the GauchoSpace 'People' page after filtering for student
+// and pressing "Show all <# of classmates>"
 [...document.querySelectorAll(`.d-inline-block.aabtn`)].map(el => el.textContent).join('\n')
